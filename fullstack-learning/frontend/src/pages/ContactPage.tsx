5cf1c46db586f92f6c0733f756d954a8295811f7
ryanblaisdell <blaisdell.ryan11@gmail.com>
2024-08-17 17:38:55 -0400

    changed file structure and updated site

---
+++ fullstack-learning/frontend/src/pages/ContactPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage: React.FC = () => {
+  return (
+    <div>
+        <p>This is the contact page.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
