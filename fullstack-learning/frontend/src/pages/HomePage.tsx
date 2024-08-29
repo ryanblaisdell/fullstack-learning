0d25af1c00e717973a74c8fc34d963c161254ba8
Ryan Blaisdell <blaisdell.ryan11@gmail.com>
2024-08-29 01:13:23 -0400

    added category enum, adding respective links for rendered tiles to redirect users to desired location, added category enums to tile rendering

--- fullstack-learning/frontend/src/pages/HomePage.tsx
+++ fullstack-learning/frontend/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Tile from '../components/Tile';
 import MainSearchBar from '../components/MainSearchBar';
 import { Item, Exercise, SearchResult } from '../interfaces';
+import { Category } from '../constants/Category';
 
 const HomePage: React.FC = () => {
   const [results, setResults] = useState<SearchResult>({ items: [], exercises: [] }); 
@@ -17,6 +18,8 @@ const HomePage: React.FC = () => {
               title={item.name}
               bodyText={`Description: ${item.description}, Price: $${item.price}`}
               pxHeight='250'
+              category={Category.Item}
+              urlLink={'www.amazon.com'}
             />
           ))}
           {results.exercises.map((exercise: Exercise) => (
@@ -24,7 +27,9 @@ const HomePage: React.FC = () => {
               key={exercise.id}
               title={exercise.name}
               bodyText={`Muscle Group: ${exercise.muscleGroup}, Difficulty: ${exercise.difficultyLevel}, Equipment: ${exercise.equpimentNeeded}`}
-              pxHeight='250'
+              pxHeight='250' 
+              category={Category.Exercise} 
+              urlLink={'www.youtube.com'}            
             />
           ))}
         </>
