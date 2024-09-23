526bb0d79f5db393270e79eb58d8c1fb7df528ae
Ryan Blaisdell <blaisdell.ryan11@gmail.com>
2024-09-23 00:04:34 -0400

    added models for users, workoutplans, users enrolled in plans, and a bridging table for exercises and equipment

--- fullstack-learning/backend/backendproject/projectapp/admin.py
+++ fullstack-learning/backend/backendproject/projectapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Item, Exercises
+from .models import Item, Exercises, Subscribers, WorkoutPlans, UserWorkoutPlans, EquipedWorkouts
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Exercises)
+admin.site.register(Subscribers)
+admin.site.register(WorkoutPlans)
+admin.site.register(UserWorkoutPlans)
+admin.site.register(EquipedWorkouts)
--- fullstack-learning/backend/backendproject/projectapp/models.py
+++ fullstack-learning/backend/backendproject/projectapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,8 +15,43 @@ class Exercises(models.Model):
     name = models.CharField(max_length=100)
     muscleGroup = models.CharField(max_length=100)
     difficulty_level = models.CharField(max_length=50, choices=[('Novice', 'Novice'), ('Intermediate', 'Intermediate'), ('Expert', 'Expert')])
-    equipment_needed = models.CharField(max_length=200, blank=True)
     instructions = models.TextField()
 
     def __str__(self):
         return self.name
+    
+class Subscribers(User):
+    def __str__(self):
+        return self.username
+    
+class WorkoutPlans(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    exercises = models.ManyToManyField(Exercises)
+
+    def __str__(self):
+        return self.name
+    
+class UserWorkoutPlans(models.Model):
+    user = models.ForeignKey(Subscribers, on_delete=models.CASCADE)
+    workout_plan = models.ForeignKey(WorkoutPlans, on_delete=models.CASCADE)
+    date_registered = models.DateField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.workout_plan.name
+    
+    def completed(self):
+        return self.completed
+    
+    def in_progress(self):
+        return not self.completed
+    
+# make a class that links the exercises to the items
+class EquipedWorkouts(models.Model):
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.exercise.name + ' - ' + self
